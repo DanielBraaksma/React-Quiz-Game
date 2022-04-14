eb5d386f4d9ac940939ad1527a4b3e3ae8d03147
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-14 14:21:03 -0400

    added welcome screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react"
 
-export default function App () {
+export default function App() {
+    const [gameStatus, setGameStatus] = React.useState("welcome")
+
+    function startGame() {
+        setGameStatus("in-progress")
+        console.log("game started")
+    }
+
     return (
-        <h1>Test</h1>
+        <main>
+            {gameStatus === "welcome" &&
+                <div className="welcome">
+                    <h1 className="title">Quiz Me</h1>
+                    <h3 className="subtitle">Test your knowledge</h3>
+                    <button className="start-btn" onClick={startGame}>Start</button>
+                </div>
+            }
+            {gameStatus === "in-progress" &&
+                <div className="quiz-container">
+                    <p>quiz has started</p>
+                </div>
+            }
+            {gameStatus === "scored" &&
+                <div className="quiz-container">
+                    <p>quiz has started</p>
+                </div>
+            }
+
+        </main>
     )
 }
