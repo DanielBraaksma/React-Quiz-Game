e92409c3c16fbc7ec145e13e537d9ed2e2850dee
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-15 12:10:07 -0400

    limit api calls, rendering questions components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react"
 import Question from "./components/Question"
+import he from "he";
 
 export default function App() {
+    const [callApi, setCallApi] = React.useState(false)
     const [gameStatus, setGameStatus] = React.useState("welcome")
+    const [questions, setQuestions] = React.useState("")
+    let questionElements;
+
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=5")
+            .then(res => res.json())
+            .then(data => setQuestions(data.results))
+        console.log( "questions set")
+
+    }, [callApi])
+
+    console.log(questions)
 
     function startGame() {
         setGameStatus("in-progress")
         console.log("game started")
+    }
+
+
+    if (questions) {
+        questionElements = questions.map(question => {
+            return <Question value={question} />
+        })
     }
 
     return (
@@ -19,11 +40,11 @@ export default function App() {
                 </div>
             }
             {gameStatus === "in-progress" &&
-                <Question />
-                
+                questionElements
             }
 
-
+            {gameStatus === "in-progress" &&
+            <button className="score-btn" onClick={()=>console.log("clicked")}>Click me</button>}
         </main>
     )
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,12 @@
 // import React from "react"
 // import Logo from "../images/logo.png"
+import he from "he";
 
-export default function Question() {
+export default function Question(props) {
+    // console.log(props)
     return (
         <div className="question-container">
-            <h4 className="question--title">Test Question</h4>
+            <h4 className="question--title">{he.decode(props.value.question)}</h4>
             <div className="question-options">
                 <p className="option">option 1</p>
                 <p className="option">option 2</p>
