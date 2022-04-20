385c33ddc031e0ff8ac9b5805f105afc2e47e484
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-19 21:37:34 -0400

    added background blob elements and made them responsive

--- src/App.js
+++ src/App.js
@@ -16,65 +16,67 @@ export default function App() {
             .then(data => {
                 let resArray = data.results;
 
-                setQuestions (resArray.map((q,i)=>{
+                setQuestions(resArray.map((q, i) => {
                     let optionsArr = [q.correct_answer, ...q.incorrect_answers].sort();
-                        return {
-                            question: q.question,
-                            correct: q.correct_answer,
-                            options: optionsArr,
-                            selectedAnswer: "",
-                            select: selectOption
-                        }
+                    return {
+                        question: q.question,
+                        correct: q.correct_answer,
+                        options: optionsArr,
+                        selectedAnswer: "",
+                        select: selectOption
+                    }
                 }))
             })
     }, [callApi])
 
     // figured out, needed to pass gamestatus state up from grandchild component.
 
-   function selectOption (option, gameStatus, question){
-    // logic for selecting each element
- if (gameStatus === "in-progress"){
-    setQuestions (prevQuestions=>prevQuestions.map(q=>{
-        if (q.question === question && q.selectedAnswer === option){return {...q, selectedAnswer: ""}} //&&q.selectedAnser
-        if(q.question === question){
-         return {...q, selectedAnswer: option}
-        } else {
-            return {...q}
+    function selectOption(option, gameStatus, question) {
+        // logic for selecting each element
+        if (gameStatus === "in-progress") {
+            setQuestions(prevQuestions => prevQuestions.map(q => {
+                if (q.question === question && q.selectedAnswer === option) { return { ...q, selectedAnswer: "" } } //&&q.selectedAnser
+                if (q.question === question) {
+                    return { ...q, selectedAnswer: option }
+                } else {
+                    return { ...q }
+                }
+            }))
         }
-    }))
- }
-}
-   function scoreQuiz () {
-    // make sure all selections made
-        for (let q of questions){
-            if (!q.selectedAnswer){
-             return alert("Please make a selection for each question")
+    }
+    function scoreQuiz() {
+        // make sure all selections made
+        for (let q of questions) {
+            if (!q.selectedAnswer) {
+                return alert("Please make a selection for each question")
             }
         }
 
         //if so then proceed to score
         setGameStatus("scored")
         let score = 0;
-        for (let q of questions){
-            if (q.selectedAnswer === q.correct){
+        for (let q of questions) {
+            if (q.selectedAnswer === q.correct) {
                 score++;
             }
         }
         setScore(score);
-   }
+    }
 
-   if (questions) {
-    questionElements = questions.map(ques=><Question key={ques.question} value={ques} gameStatus={gameStatus}/>)
-   }
+    if (questions) {
+        questionElements = questions.map(ques => <Question key={ques.question} value={ques} gameStatus={gameStatus} />)
+    }
 
     return (
         <div className="main-container">
+            <img src="./images/yellowblob.png" className="yellowblob"/>
+            <img src="./images/blueblob.png" className="blueblob"/>
             {gameStatus === "welcome" &&
                 <main>
                     <div className="welcome">
                         <h1 className="title">Quiz Me</h1>
                         <h3 className="subtitle">Test your knowledge</h3>
-                        <button className="start-btn" onClick={()=>setGameStatus("in-progress")}>Start</button>
+                        <button className="start-btn" onClick={() => setGameStatus("in-progress")}>Start</button>
                     </div>
                 </main>
             }
@@ -89,7 +91,7 @@ export default function App() {
                     {questionElements}
                     <div className="footer-container">
                         <p className="score">You scored {score}/{questions.length} correct answers</p>
-                        <button className="score-btn" onClick={()=>{setGameStatus("welcome");setCallApi(prevApi=>!prevApi)}}>Play Again</button>
+                        <button className="score-btn" onClick={() => { setGameStatus("welcome"); setCallApi(prevApi => !prevApi) }}>Play Again</button>
                     </div>
                 </main>
             }
